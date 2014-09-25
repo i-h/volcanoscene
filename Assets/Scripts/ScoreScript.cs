c0954c03734beb381b2f879396f4b6e9b3759ae9
Teemu Kokkonen <teemu@rautasydan.org>
2014-09-25 18:07:19 +0300

    Added victory screen etc

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PlayerScript.score = (int)PlayerScript.lifeTime;
+		Screen.showCursor = true;
+	}
+}
